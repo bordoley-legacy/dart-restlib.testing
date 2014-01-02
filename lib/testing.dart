0e1dfa4e347891096342694bdf72ec710521db04
David Bordoley <bordoley@gmail.com>
2014-01-02 10:28:16 -0800

    Add expectOnCompletion convenience function.

--- lib/testing.dart
+++ lib/testing.dart
@@ -1,7 +1,13 @@
 library restlib.testing;
 
+import "dart:async";
+
+import "package:unittest/matcher.dart";
 import "package:unittest/unittest.dart" as unittest;
 
 import "package:restlib_common/objects.dart";
 
 part "src/equals_tester.dart";
+
+void expectOnCompletion(final Future f, onComplete(dynamic)) =>
+    unittest.expect(f.then(onComplete), completes);
